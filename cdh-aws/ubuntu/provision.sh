d2e14d7df481da70f63d2b3f37e60f0e08d21b8e
Jazz Wang <jazzwang.tw@gmail.com>
2016-01-28 17:25:36 +0800

    * force to enable vagrant-env plugin and use 'aws' as default provider

--- cdh-aws/ubuntu/provision.sh
+++ cdh-aws/ubuntu/provision.sh
@@ -3,8 +3,6 @@ echo "[32;1mUSER=$(whoami)[0m"
 ## generate locale for Traditional Chinese
 locale-gen "zh_TW.UTF-8"
 ## setup bigtop related apt repository
-wget -q http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh/archive.key -O- | sudo apt-key add -
-wget -q http://archive.cloudera.com/cdh5/ubuntu/trusty/amd64/cdh/cloudera.list -O /etc/apt/sources.list.d/cdh5.list
+wget -q http://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm/archive.key -O- | sudo apt-key add -
+wget -q http://archive.cloudera.com/cm5/ubuntu/trusty/amd64/cm/cloudera.list -O /etc/apt/sources.list.d/cm5.list
 apt-get -y update
-## install bigtop related packages
-apt-get install -y --force-yes vim locales hadoop-conf-pseudo
